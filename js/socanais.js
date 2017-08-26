3e89c11d330a9dbdf1a57c00f93a02ee7e3c191d
soanatareportagens <soanatareportagens@gmail.com>
2017-08-26 06:55:01 -0300

    Create socanais.js

---
+++ js/socanais.js
@@ -0,0 +1,233 @@
+<div style="text-align: center;">
+<div id="plano1" class="invisivel"><!obs colocando invisivel--->
+
+<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FConversa.Afiada.Oficial%2Fvideos%2F1947961595230692%2F&show_text=0&width=560" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+</div><!--------------------------canl 1 acima-------------------------->
+
+<div id="plano2" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/oq2f55e_RQ8?rel=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canl 2 acima TV Justiça – Brasil acima-------------------------->
+
+<div id="plano3" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/o6VXMHA0Rk8?rel=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 3 acima TV Senado – Brasil acima-------------------------->
+
+<div id="plano4" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/9eGWPX-X0kM?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 4 acima TV camara – Brasil-------------------------->
+
+<div id="plano5" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/Oe2UgjYC5ww?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 5 - TV Cancao Nova - Brasil acima-------------------------->
+
+<div id="plano6" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/ob1i7c1N2u4?rel=0&amp;showinfo=0" frameborder="0" 
+	    allowfullscreen></iframe>
+</div><!------------------Canal 6 - Chef TV O Canal da Gastronomia - Brasil ascima-------------------->
+
+<div id="plano7" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/K_6mqVcUwzg?
+			autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 7 euronews (em Inglês) acima-------------------------->
+
+<div id="plano7a" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/F3uBJFwcgqo?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 7a euronews (en español) acima-------------------------->
+
+<div id="plano8" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/5dwX6a67qbk?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 8 TV Pampa - Porto Alegre/RS - Brasil-------------------------->
+
+<div id="plano9" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/RB0GDLnOXqg?rel=0&amp;showinfo=0" frameborder="0" 
+allowfullscreen></iframe>
+</div><!--------------------------canal 9 HispanTV en vivo - FULL HD-------------------------->
+
+<div id="plano10" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/_2rA6cUIoI0?rel=0&amp;showinfo=0" frameborder="0" 
+allowfullscreen></iframe>
+</div><!--------------------------canal 10 DESENHOS 24 HORAS-------------------------->
+
+<div id="plano11" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/jZ2aBW68NMg?
+	autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 11 DESENHOS 24 HORAS 2-------------------------->
+
+<div id="plano12" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/0tek2aa8hAQ?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 12 TN en Vivo - Argentina-------------------------->
+
+<div id="plano13" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/gXbW-OAN93I?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 13 CNN LIVE acima-------------------------->
+
+<div id="plano13a" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/2tbSrTNIP7s?
+	autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 13a CNN LIVE espanhol-------------------------->
+
+<div id="plano14" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/JvEzM_xe54s?rel=0&amp;showinfo=0" frameborder="0" 2
+allowfullscreen></iframe>
+</div><!--------------------------canal 14 FOX News Live ascima-------------------------->
+
+<div id="plano15" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/FK_q_Lq6Cfs?rel=0&amp;showinfo=0" frameborder="0" 
+allowfullscreen></iframe>
+</div><!--------------------------canal 15 AMAZON SAT - Brasil ascima-------------------------->
+
+<div id="plano16" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/yRhOmdNX8Eg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 16 cidade alerta- TV Record Brasil acima-------------------------->
+
+<div id="plano17" class="invisivel">
+<iframe width="90%" height="550" src="http://aovivo.ebc.com.br/embed-video.html" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 17 EBC TV Brasil-------------------------->
+
+<div id="plano18" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/kFpzwhYhNg4?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 18 RT en espanha HD-------------------------->
+
+<div id="plano19" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/WAt9ZT2CQWg?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 19 Al Jazeera English - Live acima-------------------------->
+
+<div id="plano20" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/fJzvdqCdDR4?rel=0&amp;showinfo=0" frameborder="0" 
+	    allowfullscreen></iframe>
+</div><!--------------------------canal 20 TV Camara Natal ascima-------------------------->
+
+<div id="plano21" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/y60wDzZt8yg?autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 21 Sky News - Live acima-------------------------->
+
+<div id="plano22" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/hL0sEdVJs3U?autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 22 FRANCE 24 Live acima-------------------------->
+
+<div id="plano23" class="visivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/ZmOeCjenHoA?
+	autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 23 Jornal da Cultura ascima-------------------------->
+
+<div id="plano24" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/-f3pdf5LvjY?
+	autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 24 Fox Sports acima-------------------------->
+
+<div id="plano25" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/C5b319gIuxk?
+	autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 25 Jogo Aberto acima-------------------------->
+
+<div id="plano26" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/yc702qA1CPw?
+	autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 26 C5N en VIVO acima-------------------------->
+
+<div id="plano27" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/9nMuTuy5SIs?
+	autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 27 KBS World 24 Korea acima-------------------------->
+
+<div id="plano28" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/9uFXw7vKz14?autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 28 TV ABC News (Australia) acima-------------------------->
+
+<div id="plano29" class="invisivel">
+<iframe scrolling="no" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen 
+src="http://www.fox25boston.com/popup-livestream"  width ="90%" height="550"></iframe>
+</div><!--------------------------canal 29 fox25 boston news acima-------------------------->
+
+<div id="plano30" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/MyrkXKJfNV4?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 30 India Today Live TV acima-------------------------->
+
+<div id="plano31" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/Uaz7w7WXrEo?
+rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------canal 31 Rede TVT - BRasil Seu Jornal-------------------------->
+  
+  <div id="plano32" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/lRoOuXKZsws?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 32 Bom Dia Brasil acima-------------------------->
+                                           
+<div id="plano33" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/WW18_26KOls?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 33 Jornal hoje ascima-------------------------->
+	
+<div id="plano34" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/Z6KlOfvZH90?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 34 Jornal Nacional acima-------------------------->  
+	
+	<div id="plano35" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/_-L10Lh_AsY?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 35 Fantástico acima-------------------------->
+	
+<div id="plano36" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/qJs0FEjNPV8?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 36 Web TV Brasileira acima-------------------------->
+                                                                                                               
+<div id="plano37" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/ZQ6po4A488c?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!---------------------Canal 37 TRT WORLD - İstanbul TURKEY - INGLÊS acima---------------------->
+                                                                                                               
+<div id="plano38" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/9kMRiE-bSz8?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 38 Africa24 Live (en français) acima-------------------------->
+                                                                                                               <div id="plano39" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/OMDN2dj_Eqg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------Canal 39 Transmissão ao vivo do Canal rit tv acima---------------------->
+
+<div id="plano40" class="invisivel">
+<iframe width="800" height="550" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" src="http://player.r7.com/video/i/52aaab49596f99dbbc02ab09?layout=wide432p&full=true" mozallowfullscreen webkitallowfullscreen allowfullscreen></iframe>
+</div><!--------------------------Canal 40 TV Record 24 hs - Brasil acima-------------------------->
+	
+<div id="plano40a" class="invisivel">
+<iframe width="1000" height="550" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" src="https://player.r7.com/video/i/599f8a94bc1f62705300093e?layout=wide540p&full=true" mozallowfullscreen webkitallowfullscreen allowfullscreen></iframe>
+</div><!---------------Canal 40a Assista à integra do Jornal da Record - Brasil acima--------------->
+
+<div id="plano40b" class="invisivel">
+<iframe width="800" height="550" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" src="http://player.r7.com/video/i/54ae76aa2bc243235c0079d3?layout=wide432p&full=true" mozallowfullscreen webkitallowfullscreen allowfullscreen></iframe>
+</div><!--------------------------Canal 40b Talentos e JR News acima-------------------------->
+
+<div id="plano40c" class="invisivel">
+<iframe width="1000" height="550" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" 
+src="https://player.r7.com/video/i/599f8b1fd8ea62c2a4000034?layout=wide540p&full=true" mozallowfullscreen webkitallowfullscreen allowfullscreen></iframe>
+</div><!--------------------------Canal 40c Íntegra do Jornal da Record News ascima-------------------------->
+
+<div id="plano41" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/BLG_bcbEZkc?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 41 Globo News ao vivo ascima-------------------------->
+  
+<div id="plano41a" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/kuWsqeURPCI?
+rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 41a tv Globo ao vivo ascima-------------------------->
+                                                                                                               <div id="plano42" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/0raY3aYVQSo?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 42 Aaj News Live - Pakistan acima-------------------------->
+  
+<div id="plano43" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/W3g2Jx7Q6CM?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 43 CGTN Live CGTN is China acima-------------------------->
+  
+<div id="plano43a" class="invisivel">
+<iframe width="100%" height="550" src="https://www.youtube.com/embed/zghho0FINAk?
+autoplay=0&rel=0&enablejsapi=1&playerapiid=ytplayer&wmode=transparent" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------Canal 43a CGTN en Español acima-------------------------->
+
+<div id="plano100" class="invisivel">
+                           <br/><br/><br/>
+<iframe width="310" height="260" src="#" frameborder="0" allowfullscreen></iframe>
+</div><!--------------------------anuncio-------------------------->
